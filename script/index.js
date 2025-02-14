fda41032a09a8d3945a78822bcab861a64b54e05
minhee <alsgml9154@naver.com>
2025-02-14 18:09:24 +0900

    Add files via upload

---
+++ script/index.js
@@ -0,0 +1,9 @@
+/* ===================================best swiper */
+const bestSlide = new Swiper('.best_slide', {
+    slidesPerView:4,
+    navigation: {
+        nextEl: '.best-slide .swiper-button-next',
+        prevEl: '.best-slide .swiper-button-prev',
+    }
+})
+console.log ('bestSlide');
